6ade0d348cbbb8d397072874e0982fa90ba8e9ac
andronic23 <73634527+andronic23@users.noreply.github.com>
2021-07-13 22:24:46 +0300

    Update React_Apply_Call.js

--- React_Apply_Call.js
+++ React_Apply_Call.js
@@ -20,3 +20,25 @@ pay(123456, 100.00)
 //LOG: function "pay" begin
 // paying 100 to 123456
 //LOG: function "pay" end
+
+
+function  parseInt(acountId,amount) {
+  console.log(`paying ${amount} to ${acountId}`)
+}
+function watchDog(context,target) {
+    let oldTarget = context[target.name]
+    context[target.name] = function () {
+        console.info(`LOG: function "${oldTarget.name}" begin`)
+        let result = oldTarget.call(context, ...arguments)
+        console.info(`LOG: function "${oldTarget.name}" end`)
+        return result
+    }
+
+}
+watchDog(window,parseInt)
+parseInt(123456, 100.00)
+
+//-------OUTPUT------------->
+//LOG: function "parseInt" begin
+// paying 100 to 123456
+// LOG: function "parseInt" end
