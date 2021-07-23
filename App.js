cf4723fb8698500cc6abe35557f1a98cb7090ae7
andronic23 <73634527+andronic23@users.noreply.github.com>
2021-07-23 15:05:56 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+
+class App  extends Component {
+  render() {
+    return <div className="App">
+              <h1>
+                First React JS APP
+              </h1>
+            </div>;
+  }
+}
+
+export default App;
